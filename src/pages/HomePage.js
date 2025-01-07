76e25f6eaba7251636c39040a909c0921fbc88df
DeepthiLatha <kathuladeepthilatha10@gmail.com>
2025-01-07 11:38:29 +0530

    Modified home page by adding some images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import menImage from "../assets/images/men bg.jpg"; 
+import womenImage from "../assets/images/women bg.jpg"; 
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -8,12 +10,27 @@ const HomePage = () => {
     <div className="home-page">
       <h1>Add Measurements</h1>
       <div className="measurement-options">
-        <button className="button men-button" onClick={() => navigate("/measurements/men")}>
-          Men
-        </button>
-        <button className="button women-button" onClick={() => navigate("/measurements/women")}>
-          Women
-        </button>
+        {/* Men Section */}
+        <div className="category-card">
+          <img src={menImage} alt="Men" />
+          <button
+            className="men-button"
+            onClick={() => navigate("/measurements/men")}
+          >
+            Men
+          </button>
+        </div>
+
+        {/* Women Section */}
+        <div className="category-card">
+          <img src={womenImage} alt="Women" />
+          <button
+            className="women-button"
+            onClick={() => navigate("/measurements/women")}
+          >
+            Women
+          </button>
+        </div>
       </div>
     </div>
   );
