ae6b8d5a96f263af8e4aed7e4d691a24dd5fce9a
Ajay Raju <ajayrajuadapa@gmail.com>
2025-02-03 23:06:40 +0530

    imprted fontawsome

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';  // Your main App component
-import './App.css'; 
-import reportWebVitals from './reportWebVitals';
-import 'font-awesome/css/font-awesome.min.css';  // If you're using Font Awesome
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App"; // Your main App component
+import "./App.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />  {/* Render only the App component */}
+    <App /> {/* Render only the App component */}
   </React.StrictMode>
 );
 
