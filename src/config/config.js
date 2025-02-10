1085eec97a24bcf3422b6a6893122e107cf998ed
Ajay Raju Adapa <119275120+ajayraju1@users.noreply.github.com>
2025-02-10 18:16:24 +0530

    Changed API URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: "https://9c6c-2a09-bac5-3bc8-16aa-00-242-22.ngrok-free.app/api",
+  API_BASE_URL: "https://tailorlog.onrender.com/api",
   //  "http://192.168.55.102:3500/api",
   //  "https://tailorlog.onrender.com/api",
 };
